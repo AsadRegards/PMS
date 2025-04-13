025a69fd97f88477d6e4a125917bd696ad7163b0
Asad Ullah <asadregards@gmail.com>
2025-04-14 00:10:54 +0500

    Restored Broken Dashboard

--- PMS/Dashboard.Designer.cs
+++ PMS/Dashboard.Designer.cs
@@ -28,65 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dashboard));
+            components = new System.ComponentModel.Container();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            menuStrip3 = new MenuStrip();
+            vIEWALLITEMSToolStripMenuItem = new ToolStripMenuItem();
+            vIEWALLSALESToolStripMenuItem = new ToolStripMenuItem();
+            aDDNEWITEMToolStripMenuItem = new ToolStripMenuItem();
+            aDDNEWSALEToolStripMenuItem = new ToolStripMenuItem();
             mainPanel = new Panel();
-            pictureBox3 = new PictureBox();
-            mainPanel = new Panel();
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
-            mainPanel.SuspendLayout();
+            menuStrip3.SuspendLayout();
             SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.ImageScalingSize = new Size(20, 20);
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(61, 4);
+            // 
+            // menuStrip3
+            // 
+            menuStrip3.ImageScalingSize = new Size(20, 20);
+            menuStrip3.Items.AddRange(new ToolStripItem[] { vIEWALLITEMSToolStripMenuItem, vIEWALLSALESToolStripMenuItem, aDDNEWITEMToolStripMenuItem, aDDNEWSALEToolStripMenuItem });
+            menuStrip3.Location = new Point(0, 0);
+            menuStrip3.Name = "menuStrip3";
+            menuStrip3.Size = new Size(1499, 28);
+            menuStrip3.TabIndex = 3;
+            menuStrip3.Text = "menuStrip3";
+            // 
+            // vIEWALLITEMSToolStripMenuItem
+            // 
+            vIEWALLITEMSToolStripMenuItem.Name = "vIEWALLITEMSToolStripMenuItem";
+            vIEWALLITEMSToolStripMenuItem.Size = new Size(131, 24);
+            vIEWALLITEMSToolStripMenuItem.Text = "VIEW ALL ITEMS";
+            vIEWALLITEMSToolStripMenuItem.Click += vIEWALLITEMSToolStripMenuItem_Click;
+            // 
+            // vIEWALLSALESToolStripMenuItem
+            // 
+            vIEWALLSALESToolStripMenuItem.Name = "vIEWALLSALESToolStripMenuItem";
+            vIEWALLSALESToolStripMenuItem.Size = new Size(131, 24);
+            vIEWALLSALESToolStripMenuItem.Text = "VIEW ALL SALES";
+            vIEWALLSALESToolStripMenuItem.Click += vIEWALLSALESToolStripMenuItem_Click;
+            // 
+            // aDDNEWITEMToolStripMenuItem
+            // 
+            aDDNEWITEMToolStripMenuItem.Name = "aDDNEWITEMToolStripMenuItem";
+            aDDNEWITEMToolStripMenuItem.Size = new Size(129, 24);
+            aDDNEWITEMToolStripMenuItem.Text = "ADD NEW ITEM";
+            aDDNEWITEMToolStripMenuItem.Click += aDDNEWITEMToolStripMenuItem_Click;
+            // 
+            // aDDNEWSALEToolStripMenuItem
+            // 
+            aDDNEWSALEToolStripMenuItem.Name = "aDDNEWSALEToolStripMenuItem";
+            aDDNEWSALEToolStripMenuItem.Size = new Size(129, 24);
+            aDDNEWSALEToolStripMenuItem.Text = "ADD NEW SALE";
+            aDDNEWSALEToolStripMenuItem.Click += aDDNEWSALEToolStripMenuItem_Click;
             // 
             // mainPanel
             // 
-            mainPanel.BackColor = Color.FromArgb(246, 246, 246);
             mainPanel.Location = new Point(0, 31);
-            mainPanel.Margin = new Padding(3, 2, 3, 2);
             mainPanel.Name = "mainPanel";
-            mainPanel.Size = new Size(1312, 679);
+            mainPanel.Size = new Size(1499, 916);
             mainPanel.TabIndex = 4;
-            mainPanel.Paint += mainPanel_Paint;
-            // 
-            // pictureBox3
-            // 
-            pictureBox3.BackColor = Color.FromArgb(16, 22, 59);
-            pictureBox3.Cursor = Cursors.Hand;
-            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
-            pictureBox3.Location = new Point(596, 7);
-            pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(21, 19);
-            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox3.TabIndex = 12;
-            pictureBox3.TabStop = false;
-            // 
-            // panel1
-            // 
-            mainPanel.BackColor = Color.FromArgb(16, 22, 59);
-            mainPanel.Controls.Add(pictureBox3);
-            mainPanel.Location = new Point(570, 0);
-            mainPanel.Name = "panel1";
-            mainPanel.Size = new Size(630, 29);
-            mainPanel.TabIndex = 5;
             // 
             // Dashboard
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1199, 562);
+            ClientSize = new Size(1499, 944);
             Controls.Add(mainPanel);
-            Controls.Add(mainPanel);
-            FormBorderStyle = FormBorderStyle.None;
-            Margin = new Padding(3, 2, 3, 2);
+            Controls.Add(menuStrip3);
             Name = "Dashboard";
-            StartPosition = FormStartPosition.CenterScreen;
             Text = "Dashboard";
             Load += Dashboard_Load;
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
-            mainPanel.ResumeLayout(false);
+            menuStrip3.ResumeLayout(false);
+            menuStrip3.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
-        private PictureBox pictureBox3;
+        private ContextMenuStrip contextMenuStrip1;
+        private MenuStrip menuStrip3;
+        private ToolStripMenuItem vIEWALLITEMSToolStripMenuItem;
+        private ToolStripMenuItem vIEWALLSALESToolStripMenuItem;
+        private ToolStripMenuItem aDDNEWITEMToolStripMenuItem;
+        private ToolStripMenuItem aDDNEWSALEToolStripMenuItem;
         private static Panel mainPanel;
     }
 }
